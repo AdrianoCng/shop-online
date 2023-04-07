0d877f82f325cbbf3d712a20e8739f8f18d21ef6
Adriano Cangiamila <adrianocangiamila@gmail.com>
2023-04-07 22:05:18 +0100

    listen to node events for error handling

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,15 @@ dotenv.config();
 
 import app from './server';
 
+process.on('unhandledRejection', (reason: Error) => {
+  throw reason;
+});
+
+process.on('uncaughtException', (err, origin) => {
+  console.error(`Caught exception: ${err}\n` + `Exception origin: ${origin}`);
+  process.exit(1);
+});
+
 const PORT = process.env.PORT || 8000;
 app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`);
