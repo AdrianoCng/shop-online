2629618a9f5579929a8944aacd13cb97ed60ee8a
Adriano Cangiamila <adrianocangiamila@gmail.com>
2023-04-07 21:46:11 +0100

    improve validation errors handling

---
+++ src/errors/ValidateError.ts
@@ -0,0 +1,32 @@
+import { Result, ValidationError } from 'express-validator';
+import { StatusCodes, ReasonPhrases } from 'http-status-codes';
+
+import CustomError, { ICustomError } from './CustomError';
+
+class ValidateError extends CustomError {
+  public readonly errors: Result<ValidationError>;
+
+  public readonly statusCode = StatusCodes.BAD_REQUEST;
+
+  constructor(errors: Result<ValidationError>) {
+    super();
+
+    Object.setPrototypeOf(this, new.target.prototype);
+
+    this.errors = errors;
+  }
+
+  formatErrors() {
+    return this.errors
+      .formatWith<ICustomError>(({ param, msg, value, location }) => ({
+        statusCode: this.statusCode,
+        msg: msg || ReasonPhrases.BAD_REQUEST,
+        param,
+        value,
+        location,
+      }))
+      .array();
+  }
+}
+
+export default ValidateError;
--- src/middlewares/validate.ts
+++ src/middlewares/validate.ts
@@ -1,31 +1,20 @@
 import { NextFunction, Request, Response } from 'express';
 import { ValidationChain, validationResult } from 'express-validator';
-import { StatusCodes, ReasonPhrases } from 'http-status-codes';
 
-import { ICustomError } from '../errors/CustomError';
+import ValidateError from '../errors/ValidateError';
 import tryCatch from './tryCatch';
 
 const validate = (validations: ValidationChain[]) =>
-  tryCatch(async (req: Request, res: Response, next: NextFunction) => {
+  tryCatch(async (req: Request, _res: Response, next: NextFunction) => {
     await Promise.all(validations.map((validation) => validation.run(req)));
 
-    const statusCode = StatusCodes.BAD_REQUEST;
-
-    const errors = validationResult(req).formatWith<ICustomError>(
-      ({ param, msg, value, location }) => ({
-        statusCode,
-        msg: msg || ReasonPhrases.BAD_REQUEST,
-        param,
-        value,
-        location,
-      })
-    );
+    const errors = validationResult(req);
 
     if (errors.isEmpty()) {
       return next();
     }
 
-    return res.status(statusCode).json({ errors: errors.array() });
+    return next(new ValidateError(errors));
   });
 
 export default validate;
