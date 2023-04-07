ff9c91d1d594d54eaa8cb586ae70e01a14c4a3ca
Adriano Cangiamila <adrianocangiamila@gmail.com>
2023-04-07 16:06:26 +0100

    add morgan loggin

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import cors from 'cors';
 import helmet from 'helmet';
+import morgan from 'morgan';
 
 import router from './routes';
 
@@ -9,6 +10,7 @@ const app = express();
 app.use(cors());
 app.use(helmet());
 app.use(express.json());
+app.use(morgan('dev'));
 
 app.use('/api/v1', router);
 
