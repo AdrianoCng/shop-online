50af821fa176f5b1a9045d02b9b44040d5f6bb77
Adriano Cangiamila <adrianocangiamila@gmail.com>
2023-04-11 21:20:05 +0100

    refactoring

--- src/errors/NotFound.ts
+++ src/errors/NotFound.ts
@@ -4,8 +4,6 @@ import CustomError, { ICustomError } from './CustomError';
 class NotFound extends CustomError {
   public readonly statusCode = StatusCodes.NOT_FOUND;
 
-  public readonly message: string = ReasonPhrases.NOT_FOUND;
-
   constructor(message: string = ReasonPhrases.NOT_FOUND) {
     super();
     Object.setPrototypeOf(this, new.target.prototype);
