60f8e9cd04c90e98a1e5d02412b0be90994d4ea1
Adriano Cangiamila <adrianocangiamila@gmail.com>
2023-04-23 16:43:55 +0100

    export authenticateUser from index file

--- src/middlewares/index.ts
+++ src/middlewares/index.ts
@@ -1,3 +1,4 @@
 export { default as errorHandler } from './errorHandler';
 export { default as validate } from './validate';
 export { default as tryCatch } from './tryCatch';
+export { default as authenticateUser } from './authenticateUser';
