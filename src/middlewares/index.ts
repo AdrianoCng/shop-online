2478e70f24aa047d9a5e457495694fcd001d9a1e
Adriano Cangiamila <adrianocangiamila@gmail.com>
2023-04-29 12:22:51 +0100

    paginate middleware export/import improve

--- src/middlewares/index.ts
+++ src/middlewares/index.ts
@@ -2,3 +2,4 @@ export { default as errorHandler } from './errorHandler';
 export { default as validate } from './validate';
 export { default as tryCatch } from './tryCatch';
 export { default as authenticateUser } from './authenticateUser';
+export { default as paginate } from './paginate';
--- src/routes/products.ts
+++ src/routes/products.ts
@@ -1,9 +1,8 @@
 import { Router } from 'express';
 
-import { tryCatch, validate } from '../middlewares';
+import { tryCatch, validate, paginate } from '../middlewares';
 import { productsControllers } from '../controllers';
 import { postProductValidation } from '../validations';
-import paginate from '../middlewares/paginate';
 import { Product } from '../models';
 
 const productRouter = Router();
