a357c275fcc555f28fcf188f4b9c181e56f3c738
Adriano Cangiamila <adrianocangiamila@gmail.com>
2023-04-30 01:13:16 +0100

    delete product validation

--- src/routes/products.ts
+++ src/routes/products.ts
@@ -2,7 +2,7 @@ import { Router } from 'express';
 
 import { tryCatch, validate, paginate, authenticateUser } from '@middlewares/index';
 import { productsControllers } from '@controllers/index';
-import { postProductValidation } from '@validations/index';
+import { deleteProductValidation, postProductValidation } from '@validations/index';
 import { Product } from '@models/index';
 
 const productRouter = Router();
@@ -11,6 +11,10 @@ productRouter.use(authenticateUser);
 
 productRouter.get('/', paginate(Product), tryCatch(productsControllers.getAllProducts));
 productRouter.post('/', validate(postProductValidation), tryCatch(productsControllers.postProduct));
-productRouter.delete('/:id', tryCatch(productsControllers.deleteProduct));
+productRouter.delete(
+  '/:id',
+  validate(deleteProductValidation),
+  tryCatch(productsControllers.deleteProduct)
+);
 
 export default productRouter;
--- src/validations/index.ts
+++ src/validations/index.ts
@@ -1,5 +1,4 @@
-// TODO: remove eslint-disable
-/* eslint-disable import/prefer-default-export */
 export { default as signupValidation } from './auth/signup';
 export { default as loginValidation } from './auth/login';
 export { default as postProductValidation } from './products/postProduct';
+export { default as deleteProductValidation } from './products/deleteProduct';
