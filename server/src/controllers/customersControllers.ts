a74b4d34ccc229007c4e5826a5492d8ebcb114de
Adriano Cangiamila <adrianocangiamila@gmail.com>
2023-08-24 20:57:55 +0100

    get all customers and update endpoints

--- server/src/controllers/customersControllers.ts
+++ server/src/controllers/customersControllers.ts
@@ -3,6 +3,16 @@ import { Customer } from '@models/Customer';
 import { NextFunction, Request, Response } from 'express';
 import { StatusCodes } from 'http-status-codes';
 
+const getAllCustomers = async (req: Request, res: Response, next: NextFunction) => {
+  const paginatedCustomers = req.paginatedResults;
+
+  if (!paginatedCustomers) {
+    return next(new CustomError(StatusCodes.NOT_FOUND));
+  }
+
+  return res.status(StatusCodes.OK).json(paginatedCustomers);
+};
+
 const getCustomerDetails = async (req: Request, res: Response, next: NextFunction) => {
   const customerID = req.params.id;
 
@@ -19,6 +29,28 @@ const getCustomerDetails = async (req: Request, res: Response, next: NextFunctio
   return res.status(StatusCodes.OK).json(customer);
 };
 
+const updateCustomerDetails = async (req: Request, res: Response, next: NextFunction) => {
+  const customerID = req.params.id;
+
+  if (!customerID) {
+    return next(new CustomError(StatusCodes.BAD_REQUEST));
+  }
+
+  const updatedCustomer = Customer.findByIdAndUpdate(
+    customerID,
+    { $set: req.body },
+    { new: true, runValidators: true }
+  );
+
+  if (!updatedCustomer) {
+    return next(new CustomError(StatusCodes.NOT_FOUND, 'Customer not found'));
+  }
+
+  return res.status(StatusCodes.OK).json(updatedCustomer);
+};
+
 export default {
   getCustomerDetails,
+  updateCustomerDetails,
+  getAllCustomers,
 };
--- server/src/routes/customers.ts
+++ server/src/routes/customers.ts
@@ -4,13 +4,19 @@ import { customersControllers } from '@controllers/index';
 import tryCatch from '@middlewares/tryCatch';
 import validate from '@middlewares/validate';
 import { getCustomerDetailsValidation } from '@validations/index';
+import paginate from '@middlewares/paginate';
+import { Customer } from '@models/Customer';
 
 const customersRouter = Router();
+
+customersRouter.get('/', paginate(Customer), tryCatch(customersControllers.getAllCustomers));
 
 customersRouter.get(
   '/:id',
   validate(getCustomerDetailsValidation),
   tryCatch(customersControllers.getCustomerDetails)
 );
+
+customersRouter.put('/:id', customersControllers.updateCustomerDetails);
 
 export default customersRouter;
